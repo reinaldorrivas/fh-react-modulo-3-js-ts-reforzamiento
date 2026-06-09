a4ae8239b65bbe5fc910717e596899bc0ff06858
René Rivas <reinaldorrivas@gmail.com>
2026-06-09 09:03:25 -0400

    fh-react-module-3: template strings and some minor comment fixes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import "./bases/01-const-let";
-import "./style.css";
+import './bases/01-const-let';
+import './style.css';
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /* html */ `
   <main>
     <h1>Hola Mundo</h1>
   </main>
