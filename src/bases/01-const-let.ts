8fd28b2d863d3982d8a340579924fe130c3f67f0
René Rivas <reinaldorrivas@gmail.com>
2026-06-10 11:12:11 -0400

    fh-react-module-3: Minor fix in class 1

--- src/bases/01-const-let.ts
+++ src/bases/01-const-let.ts
@@ -1,12 +1,18 @@
 // * Unless you declare the var type explicit when you're using const, the data type is the value
 // * If you need to create a var, if its value never changes create it as a const, otherwise create it as a let
-const firstName = "René";
-const lastName: string = "Rivas";
+const firstName = 'René';
+const lastName: string = 'Rivas';
 
 let diceNumber = 5;
+console.log({ diceNumber });
+
 diceNumber = 3;
 
 // * When you're using the chaining operator, you need to take care about any var type change
-const containsLetterH = lastName.toLowerCase().trim().includes('H');
+const letter = 'R';
+const containsLetterR = lastName
+  .toLowerCase()
+  .trim()
+  .includes(letter.toLowerCase().trim());
 
-console.log({ firstName, lastName, containsLetterH, diceNumber });
+console.log({ firstName, lastName, containsLetterR, diceNumber });
