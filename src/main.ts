2f683d4263960b6be0bb53b98dd2eb37095132ba
René Rivas <reinaldorrivas@gmail.com>
2026-06-17 11:48:09 -0400

    fh-react-modulo-3: Arrays base

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 // import './bases/02-template-strings';
 // import './bases/03-object-literals';
 // import './bases/04-arrays';
+// import './bases/04-arrays';
 import './bases/05-functions';
 
 import './style.css';
